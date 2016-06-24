539c9cda485915c456fc7143cc374575d63d453e
Antoine Lambert <antoine.lambert33@gmail.com>
2016-06-25 00:38:28 +0200

    some improvements regarding the copy of required assets to the IPython web server + add a toolbar over the view to switch interactors and a center view button

--- tulipnb/__init__.py
+++ tulipnb/__init__.py
@@ -17,13 +17,46 @@ from tulip import *
 
 TULIPJS_HTML = jinja2.Template("""
 <style type="text/css">
-    div.output_area .tulip_viz { height:400px; border: 1px solid black}
+    .tulip_viz {
+        height: 400px;
+        border: 1px solid black;
+    }
+    .interactor_button {
+        position: absolute;
+        left: 5px !important;
+        z-index: 10;
+    }
+    .center_button {
+        position: absolute;
+        right: 0px !important;
+        top: 0px !important;
+        z-index: 10;
+    }
 </style>
-<div id="{{ vizid }}" class="tulip_viz"></div>
+<link rel="stylesheet" href="{{ jquerytoolbarcss_url }}" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>
+<div id="toolbar-options{{ vizid }}" class="hidden">
+   <a class="interactor-znp" href="#"><i class="fa fa-hand-paper-o" aria-hidden="true"></i></a>
+   <a class="interactor-zoom" href="#"><i class="fa fa-search-plus"></i></a>
+   <a class="interactor-fisheye" href="#"><i class="fa fa-eye"></i></a>
+</div>
+<div id="{{ vizid }}" class="tulip_viz">
+<div id="toolbar{{ vizid }}" class="btn-toolbar btn-toolbar-dark interactor_button hidden">
+    <i class="fa fa-hand-paper-o" aria-hidden="true"></i>
+</div>
+<div id="center{{ vizid }}" class="btn-toolbar btn-toolbar-dark center_button hidden">
+    <i class="fa fa-arrows" aria-hidden="true"></i>
+</div>
+</div>
+
 <script type="text/javascript">
+
   require.config({paths: {tulip: "{{ tulipjs_url[:-3] }}",
-                          base64utils: "{{ base64utils_url[:-3] }}"}});
-  require(["tulip", "base64utils"], function(tulip, base64utils) {
+                          base64utils: "{{ base64utils_url[:-3] }}",
+                          jquerytoolbar: "{{ jquerytoolbarjs_url[:-3] }}"}});
+  require(["tulip", "base64utils", "jquerytoolbar"], function(tulip, base64utils) {
+
+    var tulipView;
 
     function initTulipGraphVisualization() {
       if (!tulip.isLoaded()) {
@@ -32,12 +65,56 @@ TULIPJS_HTML = jinja2.Template("""
         var tlpbgzGraphBase64 = "{{ tlpbgz_graph_base64 }}";
         var tlpbgzGraphBinary = base64utils.base64DecToArr(tlpbgzGraphBase64);
         var container = document.getElementById("{{ vizid }}");
-        var tulipView = tulip.View(container);
+        tulipView = tulip.View(container);
+        $("#toolbar{{ vizid }}").removeClass('hidden');
+        $("#center{{ vizid }}").removeClass('hidden');
         tulipView.loadGraphFromData("graph.tlpb.gz", tlpbgzGraphBinary);
         tulipView.centerScene();
         tulipView.draw();
       }
     };
+
+    $("#toolbar-options{{ vizid }}").find(".interactor-znp").on('click', function() {
+		tulipView.activateInteractor('ZoomAndPan');
+	});
+
+    $("#toolbar-options{{ vizid }}").find(".interactor-zoom").on('click', function() {
+		tulipView.activateInteractor('RectangleZoom');
+	});
+
+    $("#center{{ vizid }}").on('click', function() {
+		tulipView.centerScene();
+	});
+
+    $("#toolbar-options{{ vizid }}").find(".interactor-fisheye").on('click', function() {
+		tulipView.activateInteractor('Fisheye');
+	});
+
+    $("#toolbar-options{{ vizid }}").find('a').on('click', function() {
+		$this = $(this);
+		$button = $('#toolbar{{ vizid }}');
+		$newClass = $this.find('i').attr('class').substring(3);
+		$oldClass = $button.find('i').attr('class').substring(3);
+		if($newClass != $oldClass) {
+			$button.find('i').animate({
+				top: "+=50",
+				opacity: 0
+			}, 200, function() {
+				$(this).removeClass($oldClass).addClass($newClass).css({top: "-=100", opacity: 1}).animate({
+					top: "+=50"
+				});
+			});
+		}
+	});
+
+
+    $('#toolbar{{ vizid }}').toolbar({
+        content: '#toolbar-options{{ vizid }}',
+        position: 'bottom',
+        style: 'dark',
+        event: 'click',
+	    hideOnClick: true
+    });
 
     initTulipGraphVisualization();
 
@@ -58,27 +135,19 @@ def copyNeededFilesToWebServer():
 
     moduleDir = os.path.dirname(os.path.abspath(__file__))
 
-    tulipjs_file = os.path.join(moduleDir, 'js/tulip.js')
-    tulipjsmem_file = os.path.join(moduleDir, 'js/tulip.js.mem')
-    tulipjsdata_file = os.path.join(moduleDir, 'js/tulip.data')
-    base64utils_file = os.path.join(moduleDir, 'js/base64utils.js')
+    tulipjs_files = os.path.join(moduleDir, 'tulipjs')
 
-    required_files = [tulipjs_file, tulipjsmem_file, tulipjsdata_file, base64utils_file]
-
-    tulipjs = os.path.basename(tulipjs_file)
-    base64utilsjs = os.path.basename(base64utils_file)
+    required_files = [tulipjs_files]
 
     if nbextension:
-
-        prefix = '/nbextensions/'
 
         def _install_nbextension(extensions):
             """Wrapper for IPython.html.install_nbextension."""
             if IPython.version_info[0] >= 3:
                 for extension in extensions:
-                    install_nbextension(extension, user=True)
+                    install_nbextension(extension, user=True, verbose=0)
             else:
-                install_nbextension(extensions)
+                install_nbextension(extensions, verbose=0)
 
         try:
             _install_nbextension(required_files)
@@ -93,12 +162,18 @@ def copyNeededFilesToWebServer():
                     os.remove(dest)
             _install_nbextension(required_files)
 
+    prefix = '/nbextensions/tulipjs/'
+    urls = {}
+    urls['tulipjs'] = prefix + 'tulip.js'
+    urls['base64utilsjs'] = prefix + 'base64utils.js'
+    urls['jquerytoolbarjs'] = prefix + 'jquery.toolbar.min.js'
+    urls['jquerytoolbarcss'] = prefix + 'jquery.toolbar.css'
 
-    return prefix + tulipjs, prefix + base64utilsjs
+    return urls
 
 def getGraphVisualizationHTML(graph):
     vizid = 'fig_el' + str(int(random.random() * 1E10))
-    tulipjs_url, base64utilsjs_url = copyNeededFilesToWebServer()
+    urls = copyNeededFilesToWebServer()
     dirpath = tempfile.mkdtemp()
     tmpGraphFile = os.path.join(dirpath, 'graph.tlpb.gz')
     tlp.saveGraph(graph, tmpGraphFile)
@@ -106,8 +181,10 @@ def getGraphVisualizationHTML(graph):
     tlpbgzGraphDataBase64 = base64.b64encode(tlpbgzGraphData)
     shutil.rmtree(dirpath)
     return TULIPJS_HTML.render(vizid=vizid,
-                               tulipjs_url=tulipjs_url,
-                               base64utils_url=base64utilsjs_url,
+                               tulipjs_url=urls['tulipjs'],
+                               base64utils_url=urls['base64utilsjs'],
+                               jquerytoolbarjs_url=urls['jquerytoolbarjs'],
+                               jquerytoolbarcss_url=urls['jquerytoolbarcss'],
                                tlpbgz_graph_base64=tlpbgzGraphDataBase64)
 
 def display(graph):
